81d70c81f708a551480ee27e3fef0a74c65dd6fc
Eliza Taylor <elizataylor2002@gmail.com>
2018-07-11 14:47:56 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -5,9 +5,10 @@ RAINBOW_COLORS = [
 ]
 
 def change_rainbow_colors
- RAINBOW_COLORS[0]"red"
- RAINBOW_COLORS[1]"lighst red"
- RAINBOW_COLORS[2]"loght_yellow"
+ RAINBOW_COLORS[0]="red"
+ RAINBOW_COLORS[1]="lighst red"
+ RAINBOW_COLORS[2]="loght_yellow"
+ end
 
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
